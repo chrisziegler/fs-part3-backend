3d252a52ca43eac7ed7150bb3e8accbf862bc9e4
Chris Ziegler <chrisrziegler@gmail.com>
2020-09-23 22:52:40 -0700

    initial commit to remote

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-let notes = [
+let persons = [
   {
+    name: 'Jerry Cantrell',
+    number: '867-5309',
     id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
   },
   {
+    name: 'Jim Gaffigan',
+    number: '555-2121',
     id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
   },
   {
+    name: 'Dwight D. Eisenhauer',
+    number: '867-5310',
     id: 3,
-    content:
-      'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
+  },
+  {
+    name: 'Vladimir Putin',
+    number: '666-1212',
+    id: 4,
   },
 ]
 
@@ -32,15 +33,15 @@ let notes = [
 //   res.send('<h1>Hello World!</h1>')
 // })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
+  const foundPerson = persons.find(person => person.id === id)
+  if (foundPerson) {
+    response.json(foundPerson)
   } else {
     response.status(404).end()
   }
@@ -48,30 +49,29 @@ app.get('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
   const maxId =
-    notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
+    persons.length > 0 ? Math.max(...persons.map(note => note.id)) : 0
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
-  if (!body.content) {
-    return response.status(400).response.json({
-      error: 'content missing',
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'both name and number are required',
     })
   }
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+  const person = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
-  notes = notes.concat(note)
-  response.json(note)
+  persons = persons.concat(person)
+  response.json(person)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
