918fc81762d7b555db71402f10540e8a2d1e7274
Chris Ziegler <chrisrziegler@gmail.com>
2020-09-30 23:38:56 -0700

    updated build

---
+++ build/precache-manifest.46652bedb457a5d3eb048ccb53956a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19de463cfb55c18fd631bb6cbe9a4fd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec8b313d977f19dcc39f",
+    "url": "/static/css/main.c79f4fc6.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8b313d977f19dcc39f",
+    "url": "/static/js/main.ad9d8ec6.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
